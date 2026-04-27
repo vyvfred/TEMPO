b087b5e77a669be847330a4376b941d7803f4204
[dyad] <git@dyad.sh>
2026-04-27 17:34:58 +0200

    [dyad] Fixed Badge import in Parametres.tsx - wrote 1 file(s)

--- src/views/Parametres/Parametres.tsx
+++ src/views/Parametres/Parametres.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
+import { Badge } from '@/components/ui/badge';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { 
   Building2, User, Bell, Moon, Database, Download, 
